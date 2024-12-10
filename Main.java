8cb08f11a9c8208804f876400909e0a82cc3f063
TristiPouss <tristandsg@gmail.com>
2024-12-10 18:50:08 +0100

    Interactive main menu fully working

--- Main.java
+++ Main.java
@@ -6,28 +6,37 @@ public class Main {
     public static void main(String[] args) {
         boolean dev = false;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Chinese Postman Solver");
+        while (true){
+            System.out.print("\033\143");
+            System.out.println("Welcome to the Chinese Postman Solver\n");
 
-        UndirectedGraphChinesePostman graph = null;
+            UndirectedGraphChinesePostman graph = null;
 
-        // DOT input handling
-        System.out.print("Enter the path to the DOT file: ");
-        String filename = scanner.nextLine();
+            // DOT input handling
+            System.out.print("Enter the name of the DOT file: ");
+            String filename = scanner.nextLine();
 
-        try {
-            System.out.println("Loading graph...");
-            graph = UndirectedGraphChinesePostman.fromDotFile(filename);
+            try {
+                System.out.println("\nLoading graph...");
+                graph = UndirectedGraphChinesePostman.fromDotFile(filename);
 
-            if (dev) System.out.println(graph.toDotString());
+                if (dev) System.out.println(graph.toDotString());
 
-        } catch (Exception e) {
-            System.err.println("Error loading graph: " + e.getMessage());
+            } catch (Exception e) {
+                System.err.println("Error loading graph: " + e.getMessage());
+            }
+            System.out.println("\nSolving graph :");
+            ChinesePostman cpp = new ChinesePostman(graph);
+            cpp.solve();
+            System.out.println("\nExporting solution to '" + filename + "-processed.gv'");
+            cpp.toDotFile(filename);
+
+            System.out.println("\nWould you like to solve another graph ? (YES/no)");
+            String stop = scanner.nextLine();
+            if (stop.equals("no")) break;
         }
-
-        ChinesePostman cpp = new ChinesePostman(graph);
-        cpp.solve();
-        cpp.toDotFile(filename);
-
         scanner.close();
+        System.out.print("\033\143");
+        System.out.println("Application Closed.");
     }
 }
